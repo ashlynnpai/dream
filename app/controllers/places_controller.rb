bb0ea778d524f79c218d20aac380e2512bd525d1
Ashlynn Pai <ashlynnpai@gmail.com>
2016-07-02 02:27:46 +0000

    add places new

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,11 +1,15 @@
 class PlacesController < ApplicationController
   
   def index
-     @places = Place.paginate(:page => params[:page], :per_page => 10)
-     respond_to do |format|
+    @places = Place.paginate(:page => params[:page], :per_page => 10)
+    respond_to do |format|
       format.html
       format.json{render json: @places}
       format.xml{render xml: @places}
-     end
+    end
+  end
+  
+  def new
+    @place = Place.new
   end
 end
