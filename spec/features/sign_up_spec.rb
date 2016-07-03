325b11ea09ef845bb2786128eb49956bcb4519a1
Ashlynn Pai <ashlynnpai@gmail.com>
2016-07-03 20:07:43 +0000

    sign up with correct credentials spec

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper.rb'
+
+feature "User signs up" do
+  
+  scenario "Sign up with correct credentials" do
+    visit new_user_registration_path
+    fill_in 'Username', :with => "Happy Hippo"
+    fill_in 'Email', :with => "user1@example.com"
+    fill_in 'Password', :with => "password1", :match => :prefer_exact
+    fill_in 'Password confirmation', :with => "password1", :match => :prefer_exact
+    click_button 'Sign up'
+    expect(page).to have_content "Welcome! You have signed up successfully."
+  end
+  
+end
