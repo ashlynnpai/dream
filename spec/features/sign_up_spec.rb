471683579bd727028042f8e680bd6b8d92f75892
Ashlynn Pai <ashlynnpai@gmail.com>
2016-07-03 20:09:28 +0000

    sign up with incorrect credentials

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -12,4 +12,14 @@ feature "User signs up" do
     expect(page).to have_content "Welcome! You have signed up successfully."
   end
   
+  scenario "Sign up with incorrect credentials" do
+    visit new_user_registration_path
+    fill_in 'Username', :with => nil
+    fill_in 'Email', :with => "user1@example.com"
+    fill_in 'Password', :with => "password1", :match => :prefer_exact
+    fill_in 'Password confirmation', :with => "password1", :match => :prefer_exact
+    click_button 'Sign up'
+    expect(page).to have_content "Please review the problems below:"
+  end
+  
 end
