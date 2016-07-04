6ece6e9ab7f7a265187eabf792b80bef1bdb6c64
Ashlynn Pai <ashlynnpai@gmail.com>
2016-07-04 03:20:10 +0000

    additional validations for place

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Place, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
   
   it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:address) }
+  it { should validate_presence_of(:description) }
 
 end
