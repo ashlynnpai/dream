469e8754b0734632e5618245b5fcdc91f2742a79
Ashlynn Pai <ashlynnpai@gmail.com>
2016-07-04 21:20:22 +0000

    comments create

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe CommentsController, type: :controller do
+  
+  describe 'POST create' do
+    let(:place) { Fabricate(:place) }
+     
+    context 'with authenticated users' do
+      before do
+        @user = Fabricate(:user)
+        allow(controller).to receive(:authenticate_user!).and_return(true)
+        allow(controller).to receive(:current_user).and_return(@user)
+      end
+      
+      it 'creates a review' do
+        post :create, comment: Fabricate.attributes_for(:comment), place_id: place.id
+        expect(Comment.count).to eq(1)
+      end
+      
+      it 'redirects to the place path' do
+        post :create, comment: Fabricate.attributes_for(:comment), place_id: place.id
+        expect(response).to redirect_to place_path(place)        
+      end
+    end
+  end
+
+end
--- spec/fabricators/comment_fabricator.rb
+++ spec/fabricators/comment_fabricator.rb
@@ -1,2 +1,4 @@
 Fabricator(:comment) do
+  message { Faker::Lorem.paragraph(3) }
+  rating { (1..5).to_a.sample }
 end
