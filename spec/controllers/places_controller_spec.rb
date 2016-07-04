82c5db366ec911aa165b99f5a8f71f653c0d29c1
Ashlynn Pai <ashlynnpai@gmail.com>
2016-07-04 00:40:26 +0000

    places edit

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -28,13 +28,31 @@ describe PlacesController, type: :controller do
       expect(response).to render_template('show')
     end
   end
-
-  before do
-    @user = Fabricate(:user)
-    sign_in @user
+  
+  describe 'GET edit' do
+    before do
+      @user = Fabricate(:user)
+      sign_in @user
+    end
+    
+    it 'sets @place' do
+      place = Fabricate(:place, user_id: @user.id)
+      get :edit, id: place.id
+      expect(assigns(:place)).to eq(place)
+    end
+    
+    it 'renders the edit template' do
+      place = Fabricate(:place, user_id: @user.id)
+      get :edit, id: place.id
+      expect(response).to render_template('edit')
+    end
   end
   
   describe 'GET new' do
+    before do
+      @user = Fabricate(:user)
+      sign_in @user
+    end
     it 'sets @place' do
       get :new
       expect(assigns(:place)).to be_a_new(Place)
@@ -42,6 +60,10 @@ describe PlacesController, type: :controller do
   end
   
   describe 'POST create' do
+    before do
+      @user = Fabricate(:user)
+      sign_in @user
+    end
     it 'creates a place' do
       post :create, place: Fabricate.attributes_for(:place)
       expect(Place.count).to eq(1)
