fca4579e8679641b88a6b63278ebf2d3a5c19c06
Ashlynn Pai <ashlynnpai@gmail.com>
2016-07-07 18:25:56 +0000

    add and fix links for buckets

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -8,7 +8,7 @@ class BucketsController < ApplicationController
   def create
     @place = Place.find(params[:place_id])
     Bucket.create(place: @place, user: current_user, position: last_position, list_state: "todo") unless current_user.buckets.map(&:place).include?(@place)
-    redirect_to buckets_path
+    redirect_to bucketlist_path
   end
   
   def visited
@@ -19,7 +19,7 @@ class BucketsController < ApplicationController
     @bucket = Bucket.find(params[:id])
     @bucket.update_attributes(bucket_params)
     if @bucket.valid?
-      redirect_to buckets_path
+      redirect_to bucketlist_path
     else
       render :edit, status: :unprocessable_entity
     end
