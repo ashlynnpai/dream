5f97d7e1bcf7b2522decdd07eee088579686b14e
Ashlynn Pai <ashlynnpai@gmail.com>
2016-07-14 17:15:31 +0000

    add test to verify non-admin user cannot destroy a place

--- spec/controllers/admin/places_controller_spec.rb
+++ spec/controllers/admin/places_controller_spec.rb
@@ -2,17 +2,30 @@ require 'spec_helper'
 
 describe Admin::PlacesController, type: :controller do
   describe 'DELETE destroy' do
-    let(:user)  { Fabricate(:user) }
-    let(:admin) { Fabricate(:admin) }
-    let(:place) { Fabricate(:place, user_id: user.id) }
-    before do
+    context 'as admin' do
+      let(:user)  { Fabricate(:user) }
+      let(:admin) { Fabricate(:admin) }
+      let(:place) { Fabricate(:place, user_id: user.id) }
+      before do
         allow(controller).to receive(:authenticate_user!).and_return(true)
         allow(controller).to receive(:current_user).and_return(admin)
-    end
-    context 'as admin' do
+      end
       it 'destroys the place' do
         delete :destroy, { id: place.id }
         expect(Place.count).to eq(0)
+      end
+    end
+    
+    context 'not as admin' do
+      let(:user)  { Fabricate(:user) }
+      let(:place) { Fabricate(:place, user_id: user.id) }
+      before do
+        allow(controller).to receive(:authenticate_user!).and_return(true)
+        allow(controller).to receive(:current_user).and_return(user)
+      end
+      it 'does not destroy the place' do
+        delete :destroy, { id: place.id }
+        expect(Place.count).to eq(1)
       end
     end
   end
