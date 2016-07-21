55cbb305261c46c88a23089b9d225909f9c18b5c
Ashlynn Pai <ashlynnpai@gmail.com>
2016-07-21 01:45:36 +0000

    add form for edit comment to places show

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'places#index'
   
   resources :places do
-    resources :comments, only: :create
+    resources :comments, only: [:create]
     resources :photos, only: :create
     collection do
       get :search, to: "places#search"
