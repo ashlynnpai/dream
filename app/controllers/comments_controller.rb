be4ffd46433e114f05857acda44b565b80221afd
Ashlynn Pai <ashlynnpai@gmail.com>
2016-07-22 19:58:53 +0000

    edit comments create to verify save

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,19 @@ class CommentsController < ApplicationController
   
   def create
     @place = Place.find(params[:place_id])
-    @place.comments.create(comment_params)
-    redirect_to place_path(@place)
+    @comment = @place.comments.build(comment_params)
+    if @comment.save
+      flash[:notice] = "Your comment has been posted."
+      redirect_to place_path(@place)
+    else
+      @comments = @place.comments.reload
+      if @comment.errors.full_messages.present?
+        flash[:alert] = @comment.errors.full_messages[0]
+      else
+        flash[:alert] = "Your comment did not post"
+      end
+      redirect_to :back
+    end
   end
   
   private
@@ -13,3 +24,6 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:message, :rating).merge(user: current_user)
   end
 end
+
+
+  
