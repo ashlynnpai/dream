036ce414d969bf8bfd9eb9ea107502569db7de5a
Ashlynn Pai <ashlynnpai@gmail.com>
2016-07-23 03:31:00 +0000

    separate statics index and places index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: "registrations", passwords: "passwords", sessions: "sessions" }
-  root 'places#index'
+  root 'statics#index'
   
   resources :places do
     resources :comments, only: [:create, :update]
