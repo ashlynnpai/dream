6ca03201396fb2346515e6a3242dd1507ec6c5c0
Ashlynn Pai <ashlynnpai@gmail.com>
2016-07-25 16:42:45 +0000

    debug test for send comment email and config mailer url

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
+  after_create :send_comment_email
   
   validates :message, presence: true
+  
+  def send_comment_email
+    #if user.notify_on_comment == true
+      NotificationMailer.comment_added(self).deliver
+    #end
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe Comment, type: :model do
   
   it { should validate_presence_of(:message) }
   
+  describe 'send_comment_email' do
+    let(:user) { Fabricate(:user, notify_on_comment: true) }
+    let(:place) { Fabricate(:place, user_id: user.id) }
+    let(:commenter) { Fabricate(:user) }
+    
+    it 'sends an email' do
+      comment = Fabricate(:comment, user_id: commenter.id, place_id: place.id)
+      expect { comment.send_comment_email }
+      .to change { ActionMailer::Base.deliveries.count }.by(1)
+    end  
+  end
 end
