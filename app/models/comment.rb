97b26c9828d272e8edd8d5a7ab32c8aec22dbb0e
Ashlynn Pai <ashlynnpai@gmail.com>
2016-07-28 02:50:41 +0000

    handle place user nil in send_comment_email

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,13 +1,15 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
-  #after_create :send_comment_email
+  after_create :send_comment_email
   
   validates :message, presence: true
   
   def send_comment_email
-    if place.user.notify_on_comment == true
-      NotificationMailer.comment_added(self).deliver_now
-    end
+    if place.user
+      if place.user.notify_on_comment == true
+        NotificationMailer.comment_added(self).deliver_now
+      end
+    end  
   end
 end
