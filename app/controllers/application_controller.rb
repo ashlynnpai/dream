b87afa7fab64a34c017cf289e346e8242f60f6bc
Ashlynn Pai <ashlynnpai@gmail.com>
2016-07-29 18:32:35 +0000

    store location to redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :store_current_location, :unless => :devise_controller?
 
   protected
 
@@ -11,6 +12,14 @@ class ApplicationController < ActionController::Base
    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
    #devise_parameter_sanitizer.permit(:account_update, keys: [:email])
+  end
+
+  private
+  # override the devise helper to store the current location so we can
+  # redirect to it after loggin in or out. This override makes signing in
+  # and signing up work automatically.
+  def store_current_location
+    store_location_for(:user, request.url)
   end
 end
 
