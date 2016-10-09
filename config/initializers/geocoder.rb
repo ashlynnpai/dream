a47a8d3597c76891dc20475afbc7364d088e9005
Ashlynn Pai <ashlynnpai@gmail.com>
2016-10-09 02:05:49 +0000

    add_stub to geocoder and edit fabricator

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,3 +2,19 @@ Geocoder.configure(
   :lookup => :bing,
   :api_key => ENV['GEOCODER_API_KEY'],
 )
+
+Geocoder.configure(:lookup => :test)
+
+Geocoder::Lookup::Test.add_stub(
+  "New York, NY", [
+    {
+      'latitude'     => 40.7143528,
+      'longitude'    => -74.0059731,
+      'address'      => 'New York, NY, USA',
+      'state'        => 'New York',
+      'state_code'   => 'NY',
+      'country'      => 'United States',
+      'country_code' => 'US'
+    }
+  ]
+)
